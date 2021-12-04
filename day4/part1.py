983c9af7174e93ef4bf3f55e53f78a213f938f0b
Chuka Nwobi <chuka@circleci.com>
2021-12-04 23:36:23 +0100

    trade space for time

--- day4/part1.py
+++ day4/part1.py
@@ -1,14 +1,18 @@
+
 from typing import List
 
 
 class Board:
-    def __init__(self, all_numbers, grid):
-        self.all_numbers = all_numbers
+    def __init__(self, grid):
         self.grid = grid
         self.already_won = False
+        self.number_dict = {}
+        for row in range(5):
+            for col in range(5):
+                self.number_dict[grid[row][col]] = [row, col]
 
     def contains(self, draw):
-        return draw in self.all_numbers
+        return draw in self.number_dict
 
     def is_complete_row(self, row):
         if self.grid[row][0] == 'x' and self.grid[row].count(self.grid[row][0]) == 5:
@@ -25,13 +29,10 @@ class Board:
             return True
         return False
 
-    def is_marked_and_winner(self, draw):
-        for row in range(len(self.grid)):
-            for col in range(len(self.grid[0])):
-                if self.grid[row][col] == draw:
-                    self.grid[row][col] = 'x'
-                    return self.is_complete_col(col) or self.is_complete_row(row)
-        return False
+    def is_winner(self, draw):
+        row, col = self.number_dict[draw]
+        self.grid[row][col] = 'x'
+        return self.is_complete_col(col) or self.is_complete_row(row)
 
     def sum_non_marked(self):
         total = 0
@@ -52,20 +53,17 @@ def read_input(filename):
 
 def generate_boards(lines):
     current_board = []
-    all_numbers = set()
     boards: List[Board] = []
     for line in lines[1:]:
         if not line and not current_board:
             continue
         if not line:
-            boards.append(Board(all_numbers, current_board))
+            boards.append(Board(current_board))
             current_board = []
-            all_numbers = set()
             continue
         row = line.split()
         current_board.append(row)
-        all_numbers.update(row)
-    boards.append(Board(all_numbers, current_board))
+    boards.append(Board(current_board))
     return lines[0].split(','), boards
 
 
@@ -74,7 +72,7 @@ def find_bingo(filename):
     draws, boards = generate_boards(lines)
     for draw in draws:
         for board in boards:
-            if board.contains(draw) and board.is_marked_and_winner(draw):
+            if board.contains(draw) and board.is_winner(draw):
                 return board.sum_non_marked() * int(draw)
 
 
@@ -84,9 +82,7 @@ def find_bingo2(filename):
     latest = 0
     for draw in draws:
         for index, board in enumerate(boards):
-            if board.already_won:
-                continue
-            if board.contains(draw) and board.is_marked_and_winner(draw):
+            if not board.already_won and board.contains(draw) and board.is_winner(draw):
                 latest = board.sum_non_marked() * int(draw)
     return latest
 
